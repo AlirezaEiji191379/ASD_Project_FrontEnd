3ba645ecb7aaf5bba03675f3e52cb96cb152a9e5
rrosta <rrosta@yahoo.com>
2023-03-04 22:21:10 +0330

    Add Column

--- src/_components/Column.js
+++ src/_components/Column.js
@@ -1,19 +1,21 @@
 import React from "react";
-
+import Task from "./Task";
 
 const Column = ({title}) => {
     return(
         <div style= {styles.container}>
         <h3>{title}</h3>
+        <Task />
         </div>
     )
 }
 
 const styles = {
     container: {
-        backgroundColor: "#ccc",
+        backgroundColor: "#ECEBEB",
         borderRadius: 3,
-        width: 300
+        width: 300,
+        padding: 6
     }
 }
 
