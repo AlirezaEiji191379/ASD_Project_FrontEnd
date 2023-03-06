d12d78ee5357f80ac405d427cce697e82f37f1e1
mtndaghyani <matin.daghyani@gmail.com>
2023-03-06 12:44:50 +0330

    Add profile services

---
+++ src/_constants/paths.js
@@ -0,0 +1,4 @@
+export const profilePaths ={
+    USER_PROFILE_GET : '',
+    USER_PROFILE_UPDATE: ''
+}
---
+++ src/_services/profileServices.js
@@ -0,0 +1,41 @@
+import {profilePaths} from "../_constants/paths";
+
+function handleResponse(response) {
+    /*
+    Process the response received by fetch().
+    */
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        // TODO: How does backend inform us about user not exit? (Matin, )
+        if (!response.ok) {
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
+
+function getUserInfo(email) {
+    const requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({email})
+    };
+    return fetch(profilePaths.USER_PROFILE_GET, requestOptions).then(handleResponse);
+}
+
+function updateUserProfile(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({user: user})
+    };
+    return fetch(profilePaths.USER_PROFILE_UPDATE, requestOptions).then(handleResponse);
+}
+
+export const profileServices = {
+    getUserInfo: getUserInfo,
+    updateUserProfile: updateUserProfile
+}
+
